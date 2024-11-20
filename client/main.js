098a4eab62048043fa38cb1af2fce4f7a89c24c3
akman12914 <rnlduadns98@naver.com>
2024-11-20 13:32:43 +0900

    jujeob 완성

--- client/main.js
+++ client/main.js
@@ -1,10 +1,9 @@
 import data from "./data/data.js";
-import { getNode as $, getRandom, insertLast, clearContents } from "./lib/index.js";
+import { getNode as $, getRandom, addClass, removeClass, insertLast, clearContents, showAlert, isNumericString, shake, copy } from "./lib/index.js";
 
 const submit = $("#submit");
 const nameField = $("#nameField");
 const result = $(".result");
-
 console.log(data("경준오"));
 
 function handleSubmit(e) {
@@ -14,8 +13,33 @@ function handleSubmit(e) {
   const list = data(name);
   const pick = list[getRandom(list.length)];
 
+  if (!name || name.replaceAll(" ", "") === "") {
+    // console.error("올바른 양식의 이름을 입력하세요");
+    // alert("올바른 양식의 이름을 입력하세요");
+    showAlert(".alert-error", "공백은 허용하지 않습니다.", 500);
+    // addClass(nameField, "shake");
+    shake(nameField);
+
+    return;
+  }
+
+  if (isNumericString(name) === false) {
+    showAlert(".alert-error", "제대로 된 이름을 입력하세요", 500);
+    shake(nameField);
+    return;
+  }
+
   clearContents(result);
 
   insertLast(result, pick);
 }
+
+function handleCopy() {
+  const text = this.textContent;
+
+  copy(text).then(() => {
+    showAlert(".alert-success", "클립보드 복사 완료@");
+  });
+}
 submit.addEventListener("click", handleSubmit);
+result.addEventListener("click", handleCopy);
