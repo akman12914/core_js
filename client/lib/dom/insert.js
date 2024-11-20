a67ec964cc48f68659cafe785904f9662dde0b48
akman12914 <rnlduadns98@naver.com>
2024-11-20 10:34:45 +0900

    jujeob

--- client/lib/dom/insert.js
+++ client/lib/dom/insert.js
@@ -1,69 +1,22 @@
-// named export           =>  import { getNode as $, insertLast } from '..'
-// default export         =>  import ... from '..'
+import { isString } from "../utils/type.js";
+import { getNode } from "./getNode.js";
 
-// import { getNode as $, getNodes } from './lib/dom/getNode.js';
-// import { insertLast } from './lib/dom/insert.js';
-
-// import clearContents from "./lib/dom/clearContents.js";
-
-import { getNode as $, getNodes, typeError, insertLast, clearContents } from "./lib/index.js";
-
-function phase1() {
-  // 1. input 선택하기
-  // 2. input 이벤트 바인딩
-  // 3. input의 value 값 가져오기
-  // 4. 숫자 더하기
-  // 5. result에 출력하기
-
-  const first = $("#firstNumber");
-  const second = $("#secondNumber");
-  const result = $(".result");
-  const clear = $("#clear");
-
-  function handleInput() {
-    const firstValue = Number(first.value);
-    const secondValue = +second.value;
-    const total = firstValue + secondValue;
-
-    clearContents(result);
-    insertLast(result, total);
-  }
-
-  function handleClear(e) {
-    e.preventDefault();
-
-    clearContents(first);
-    clearContents(second);
-    result.textContent = "-";
-  }
-
-  first.addEventListener("input", handleInput);
-  second.addEventListener("input", handleInput);
-  clear.addEventListener("click", handleClear);
+export function insertBefore(node, text) {
+  if (isString(node)) node = getNode(node);
+  node.insertAdjacentHTML("beforebegin", text);
 }
 
-function phase2() {
-  const calculator = $(".calculator");
-  const result = $(".result");
-  const clear = $("#clear");
-  const numberInputs = [...document.querySelectorAll("input:not(#clear)")];
-
-  function handleInput() {
-    const total = numberInputs.reduce((acc, cur) => acc + Number(cur.value), 0);
-
-    clearContents(result);
-    insertLast(result, total);
-  }
-
-  function handleClear(e) {
-    e.preventDefault();
-
-    numberInputs.forEach(clearContents);
-    result.textContent = "-";
-  }
-
-  calculator.addEventListener("input", handleInput);
-  clear.addEventListener("click", handleClear);
+export function insertFirst(node, text) {
+  if (isString(node)) node = getNode(node);
+  node.insertAdjacentHTML("afterbegin", text);
 }
 
-phase2();
+export function insertLast(node, text) {
+  if (isString(node)) node = getNode(node);
+  node.insertAdjacentHTML("beforeend", text);
+}
+
+export function insertAfter(node, text) {
+  if (isString(node)) node = getNode(node);
+  node.insertAdjacentHTML("afterend", text);
+}
--- client/main.js
+++ client/main.js
@@ -1,31 +1,21 @@
-// named export           =>  import { } from '..'
-// default export         =>  import ... from '..'
+import data from "./data/data.js";
+import { getNode as $, getRandom, insertLast, clearContents } from "./lib/index.js";
 
-// import { getNode as $, getNodes } from './lib/dom/getNode.js';
-// import { insertLast } from './lib/dom/insert.js';
-
-// import clearContents from "./lib/dom/clearContents.js";
-
-import { getNode as $, getNodes, typeError, insertLast, clearContents } from "./lib/index.js";
-
-// 1. input 선택하기
-// 2. input 이벤트 바인딩
-// 3. input의 value 값 가져오기
-// 4. 숫자 더하기
-// 5. result에 출력하기
-
-const first = $("#firstNumber");
-const second = $("#secondNumber");
+const submit = $("#submit");
+const nameField = $("#nameField");
 const result = $(".result");
 
-function handleInput() {
-  const firstValue = Number(first.value);
-  const secondValue = +second.value;
-  const total = firstValue + secondValue;
+console.log(data("경준오"));
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const name = nameField.value;
+  const list = data(name);
+  const pick = list[getRandom(list.length)];
 
   clearContents(result);
-  insertLast(result, total);
-}
 
-first.addEventListener("input", handleInput);
-second.addEventListener("input", handleInput);
+  insertLast(result, pick);
+}
+submit.addEventListener("click", handleSubmit);
